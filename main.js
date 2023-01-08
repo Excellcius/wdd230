a39459a498daaf39855be933f875ee962d816061
Excellcius <107582376+Excellcius@users.noreply.github.com>
2023-01-07 23:43:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let date = new Date();
 let year = date.getFullYear();
 
-document.querySelector('h1').innerHTML = year + '&copy';
+document.querySelector('h3').innerHTML = year + '&copy';
 
 let currentDate = document.lastModified
 
